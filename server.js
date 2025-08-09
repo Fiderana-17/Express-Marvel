555b392435fb0d99d344d08260a279b9ff22f935
unknown <Fiderana Rakoto>
2025-08-09 07:48:11 +0300

    premier commit

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import fs from 'fs';
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+
+let characters = [];
+try {
+  const data = fs.readFileSync('./user.json', 'utf-8');
+  const json = JSON.parse(data);
+  characters = json.characters || [];
+} catch (e) {
+  characters = [];
+}
+let nextId = characters.length > 0 ? Math.max(...characters.map(c => c.id)) + 1 : 1;
+
+
